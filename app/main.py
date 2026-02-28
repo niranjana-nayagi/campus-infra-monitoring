afe5468c4cfe141d3c78c46c12b117699e693729
Niranjana Nayagi <niranjananayagi@gmail.com>
2026-02-28 23:41:30 +0530

    Final updates before CI run

--- app/main.py
+++ app/main.py
@@ -1,17 +1,15 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.models import incident, resource
+from app.routes import incident_routes, resource_routes
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(incident_routes.router)
+app.include_router(resource_routes.router)
+
 @app.get("/")
 def read_root():
     return {"message": "Campus Infrastructure Monitoring System Running"}
-
-from app.routes import incident_routes
-app.include_router(incident_routes.router)
-
-from app.routes import resource_routes
-app.include_router(resource_routes.router)
